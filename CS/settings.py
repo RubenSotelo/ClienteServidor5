4126e96e8d58f49ad72575715e430a15d403a34b
RubenSotelo <ryuk200312@gmail.com>
2020-06-09 04:38:15 -0500

    Secret

--- CS/settings.py
+++ CS/settings.py
@@ -91,8 +91,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
+-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -110,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
