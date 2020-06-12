1ca5519f53007bdb263aded2dea1ee312dea3d16
RubenSotelo <ryuk200312@gmail.com>
2020-06-11 20:57:24 -0500

    get_swagger2

--- CS/urls.py
+++ CS/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url('swagger/', schema_view)
+    
 ]
