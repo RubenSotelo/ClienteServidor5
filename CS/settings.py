74e039c2a51523ebf37f5e1d8a15c788f36532b9
RubenSotelo <ryuk200312@gmail.com>
2020-06-13 14:19:05 -0500

    database

--- CS/settings.py
+++ CS/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'CS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd606h257be2jvm',
+        'USER': 'grklbljrjemmhz',
+        'PASSWORD': '97a3fa3313a2a5576dd30905e390df47bb39c1d60eed67e1dc78dda1b350423f'
+        'HOST': 'ec2-18-232-143-90.compute-1.amazonaws.com'
+        'PORT': '5432'
     }
 }
 
