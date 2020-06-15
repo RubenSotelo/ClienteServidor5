73fc51b948b718374bba5896b5f6527f39a8c68c
RubenSotelo <ryuk200312@gmail.com>
2020-06-15 14:58:45 -0500

    staticFiles

--- CS/urls.py
+++ CS/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from django.conf.urls import include #url
+from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from django.conf.urls import url
