8ae31a628b9ebab62d36df4603036d6b7c0da7c5
RubenSotelo <ryuk200312@gmail.com>
2020-06-15 18:45:00 -0500

    verificacion

--- CS/urls.py
+++ CS/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     re_path(r'^', include(router.urls)),
     re_path(r'^api/v1/login', include('Login.urls')),
     re_path(r'^api/v1/example/', include('Example1.urls')),
-    re_path('swagger/', schema_view),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    re_path('swagger/', schema_view),
     
 ]
