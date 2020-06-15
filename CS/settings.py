62454e5bbd04fddbe6a94aa77333871e610d1627
RubenSotelo <ryuk200312@gmail.com>
2020-06-15 16:45:58 -0500

    verificacion

--- CS/settings.py
+++ CS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['sotelocs.herokuapp.com']
 
 
 # Application definition
